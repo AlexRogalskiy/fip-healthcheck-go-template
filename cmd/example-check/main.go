c26bd1f48d98cf0dfdbdb84d923265d337c18530
Ángel Barrera Sánchez <angel@sighup.io>
2021-07-01 12:05:17 +0200

    Add logging default flag

--- cmd/example-check/main.go
+++ cmd/example-check/main.go
@@ -10,18 +10,40 @@ import (
 
 	internal "github.com/sighupio/fip-healthcheck-go-template/internal/example-check"
 	pkg "github.com/sighupio/fip-healthcheck-go-template/pkg/example-check"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
+var logLevel string
+
 var rootCmd = &cobra.Command{
-	Use:   "example-check",
-	Short: "example-check TBD",
-	Long:  "TBD",
+	PersistentPreRunE: cmdConfig,
+	Use:               "example-check",
+	Short:             "example-check TBD",
+	Long:              "TBD",
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do business logic
 		internal.Hello()
 		pkg.Hello()
 	},
+}
+
+func cmdConfig(cmd *cobra.Command, args []string) error {
+	lvl, err := log.ParseLevel(logLevel)
+	if err != nil {
+		log.WithField("log-level", logLevel).Fatal("incorrect log level")
+
+		return fmt.Errorf("incorrect log level")
+	}
+
+	log.SetLevel(lvl)
+	log.WithField("log-level", logLevel).Debug("log level configured")
+
+	return nil
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "info", "logging level (debug, info...)")
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sighupio/fip-healthcheck-go-template
 
 go 1.16
 
-require github.com/spf13/cobra v1.1.3
+require (
+	github.com/sirupsen/logrus v1.2.0
+	github.com/spf13/cobra v1.1.3
+)
